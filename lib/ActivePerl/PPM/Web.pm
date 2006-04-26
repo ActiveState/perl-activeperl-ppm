a0c7b6c2611cb7147db66677b2acc3d0f6c9d2e6
Gisle Aas <gisle@ActiveState.com>
2006-04-26 02:43:44 -0800

    Log all web requests.
    [git-p4: depot-paths = "//depot/main/Camel/src/as-mod/ActivePerl-PPM/main/": change = 262082]

--- lib/ActivePerl/PPM/Web.pm
+++ lib/ActivePerl/PPM/Web.pm
@@ -10,13 +10,25 @@ my $ua;
 
 sub web_ua {
     return $ua ||= do {
-        require LWP::UserAgent;
-	LWP::UserAgent->new(
+	ActivePerl::PPM::Web::UA->new(
 	    agent => "PPM/$ActivePerl::PPM::VERSION ($^O) ",
 	    env_proxy => 1,
 	    keep_alive => 1,
         );
     };
+}
+
+package ActivePerl::PPM::Web::UA;
+
+use base 'LWP::UserAgent';
+use ActivePerl::PPM::Logger qw(ppm_log);
+
+sub simple_request {
+    my $self = shift;
+    my $req = shift;
+    my $res = $self->SUPER::simple_request($req, @_);
+    ppm_log("INFO", sprintf("%s %s ==> %s", $req->method, $req->uri, $res->status_line));
+    return $res;
 }
 
 1;
