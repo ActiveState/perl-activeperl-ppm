2c4ba23a9d0f110b89abe5280c78a5f23677ce4c
Gisle Aas <gisle@ActiveState.com>
2006-06-21 22:37:44 -0800

    Run GUI in the background with wperl
    [git-p4: depot-paths = "//depot/main/Camel/src/as-mod/ActivePerl-PPM/main/": change = 264751]

--- ppm.bat
+++ ppm.bat
@@ -2,4 +2,12 @@
 rem Make it easy to run ppm from the development tree
 rem without installing it first.
 
+if "%1"=="" goto wperl
+
 perl -Ilib bin\ppm %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto end
+
+:wperl
+start wperl -Ilib bin\ppm
+
+:end
