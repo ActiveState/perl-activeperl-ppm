9353a3893166e24ab5c7df8e06ca338a7c6ef237
Gisle Aas <gisle@ActiveState.com>
2006-11-28 06:54:35 -0800

    Don't remove 'site' from @INC, just move it to the back. After this we can remove our PPM::Repositories hacks and ppm should also be able to access https:// repos, if the user installs Crypt-SSLeay (bug 52508).
    [git-p4: depot-paths = "//depot/main/Camel/src/as-mod/ActivePerl-PPM/main/": change = 272636]

--- lib/ActivePerl/PPM/limited_inc.pm
+++ lib/ActivePerl/PPM/limited_inc.pm
@@ -1,5 +1,5 @@
 #
-# This module tweak @INC so that PPM will only pick up modules from
+# This module tweak @INC so that PPM will prefer to pick up modules from
 # the same install area as itself lives in.  This make it safe to install
 # upgrades to packages that PPM depend in the 'site' or 'user' areas.
 #
@@ -9,7 +9,7 @@
 (my $dir = __FILE__) =~ s,/ActivePerl/PPM/.*,,;
 
 if (grep $_ eq $dir, @INC) {
-    shift(@INC) while $INC[0] ne $dir;
+    push(@INC, shift(@INC)) while $INC[0] ne $dir;
 }
 
 1;
