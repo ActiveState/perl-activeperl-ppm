7631be5e375f3678e07409fb4465bd0d3697665a
Gisle Aas <gisle@ActiveState.com>
2006-12-08 06:41:00 -0800

    Add a few handy utilities; join_with_and and join_with_or
    [git-p4: depot-paths = "//depot/main/Camel/src/as-mod/ActivePerl-PPM/main/": change = 273187]

--- lib/ActivePerl/PPM/Util.pm
+++ lib/ActivePerl/PPM/Util.pm
@@ -3,7 +3,7 @@ package ActivePerl::PPM::Util;
 use strict;
 use base 'Exporter';
 
-our @EXPORT_OK = qw(is_cpan_package clean_err);
+our @EXPORT_OK = qw(is_cpan_package clean_err join_with_and join_with_or);
 
 sub is_cpan_package {
     my $pkg_name = shift;
@@ -19,6 +19,21 @@ sub clean_err {
     $err =~ s/ _at / at /g; # escape for when you really want "at" in the message
     $err =~ s/\n*\z//;
     return $err;
+}
+
+sub join_with_and {
+    _join_with("and", @_);
+}
+
+sub join_with_or {
+    _join_with("or", @_);
+}
+
+sub _join_with {
+    my $conjunc = shift;
+    my $text = pop(@_);
+    $text = join(" $conjunc ", join(", ", @_), $text) if @_;
+    return $text;
 }
 
 1;
---
+++ t/util.t
@@ -0,0 +1,18 @@
+#!perl -w
+
+use strict;
+use Test qw(plan ok);
+plan tests => 8;
+
+use ActivePerl::PPM::Util qw(is_cpan_package clean_err join_with_and join_with_or);
+
+ok(is_cpan_package("Foo"), "Foo");
+ok(is_cpan_package("ActivePerl-Foo"), "");
+
+ok(clean_err("foo at x.pl line 32"), "foo");
+ok(clean_err("foo _at bar"), "foo at bar");
+
+ok(join_with_and("foo"), "foo");
+ok(join_with_and("foo", "bar"), "foo and bar");
+ok(join_with_and("foo", "bar", "baz"), "foo, bar and baz");
+ok(join_with_or(1..10), "1, 2, 3, 4, 5, 6, 7, 8, 9 or 10");
